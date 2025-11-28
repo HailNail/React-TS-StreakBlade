e8eafa1e56f643e43c14651fc1d8cc534c78c796
Nail Gati <theproducer228@gmail.com>
2025-11-28 21:28:59 +0500

    little fix

--- src/stores/useHabitsStore.ts
+++ src/stores/useHabitsStore.ts
@@ -22,7 +22,7 @@ export const useHabitsStore = create<HabitStore>((set, get) => ({
     const newHabit: Habit = {
       id: Date.now(),
       name,
-      lastReset: new Date(Date.now() - 7 * 24 * 60 * 60 * 999.98).toISOString(),
+      lastReset: new Date().toISOString(),
       maxStreak: 0,
     };
     set({ habits: [...get().habits, newHabit] });
